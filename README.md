c59b092265d396da3458b150fdff0501078057b5
Roberto <jrggroberto@gmail.com>
2024-01-16 06:29:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Criando componentes HTML com JS
+
+
+https://web.dio.me/course/trabalhando-com-web-components-no-front-end/learning/4ec51d1f-04c7-49c9-8ccc-7f05e537ab72?back=/track/coding-future-banco-pan-desenvolvimento-frontend-com-angular&tab=undefined&moduleId=undefined
---
+++ src/Components/TituloDinamico.js
@@ -0,0 +1,25 @@
+class TituloDinamico extends HTMLElement{
+  constructor(){
+    super();
+
+    const shadow = this.attachShadow({ mode: "open"});
+    
+    // base do componente
+    const componenteRoot = document.createElement("h1");
+    componenteRoot.textContent = this.getAttribute("titulo");
+
+    //estilizar o componente
+    const style = document.createElement("style");
+    style.textContent = `
+    h1{
+      color: blue;
+    }
+    `;
+
+    //enviar para a shadow
+    shadow.appendChild(componenteRoot);
+    shadow.appendChild(style);
+  }
+}
+// é necessário que o tagname tenha um hifem "-"no nome
+customElements.define("titulo-dinamico", TituloDinamico);
